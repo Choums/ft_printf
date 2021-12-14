cd883f36ddc51e59848bdea3f5969ccb635d816a
Chams <65638252+Choums@users.noreply.github.com>
2021-12-14 20:35:51 +0100

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -26,15 +26,15 @@ size_t	ft_parsing(char c, args)
 
 	}
 	else if (c == 'd')
-		value += ft_putnbr_fd(ft_itoa(va_args(args, int)), 1);
+		value += ft_putnbr_fd(ft_itoa(va_arg(args, int)), 1);
 	else if (c == 'i')
-		value += ft_putnbr_fd(ft_itoa(va_args(args, int)), 1);
+		value += ft_putnbr_fd(ft_itoa(va_arg(args, int)), 1);
 	else if (c == 'u')
-		value += ft_putnbr_fd(va_args(args, int), 1);
+		value += ft_putnbr_fd(va_arg(args, int), 1);
 	else if (c == 'x')
-		value += ft_putnbr_fd(ft_itoa_hexa(va_args(args, int), HEXA), 1);
+		value += ft_putnbr_fd(ft_itoa_hexa(va_arg(args, int), HEXA), 1);
 	else if (c == 'X')
-		value += ft_putnbr_fd(ft_itoa_hexa(va_args(args, int), ft_strupcase(HEXA)), 1);
+		value += ft_putnbr_fd(ft_itoa_hexa(va_arg(args, int), ft_strupcase(HEXA)), 1);
 	else if (c == '%')
 		value += ft_putchar_fd(c, 1);
 	else
